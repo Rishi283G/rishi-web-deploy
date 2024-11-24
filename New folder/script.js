8506058ec8abeae5f31b913539c55d71c8d92ea1
Rishi283 <141485291+Rishi283G@users.noreply.github.com>
2024-11-24 12:12:11 +0530

    Add files via upload

---
+++ New folder/script.js
@@ -0,0 +1,25 @@
+// const userName = prompt(`Enter Your Name`)
+// const userAge = prompt(`Enter your age`)
+// console.log(`Name: ${userName}`);
+//     console.log(`Age: ${userAge}`);
+
+// if(userAge >= 18 && userAge <= 24 ){
+//     console.log(` ${userName}  is a Working Professional`);
+// }
+
+// if(userAge >=0 && userAge <= 4){
+//     console.log(`${userName} is a child`);
+// }
+
+// if(userAge >= 25 && userAge <= 64){
+//     console.log(`${userName} is an adult`);
+// }
+
+// if(userAge >= 65 && userAge <= 100){
+//     console.log(`${userName} is an elderly person`);
+// }
+
+// if(userAge <= 0 || userAge == undefined){
+//     console.log(`Invalid Age. Please enter a positive integer.`);
+// }
+// console.log(`End program`)
